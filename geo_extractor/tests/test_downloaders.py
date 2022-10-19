2e4b170c67e07a6743508b821f420b68104eeeb0
Anonymous <none@none.none>
2022-10-19 23:29:16 +0200

    tests: Mark internet-only tests as skip

--- geo_extractor/tests/test_downloaders.py
+++ geo_extractor/tests/test_downloaders.py
@@ -1,3 +1,4 @@
+import pytest
 from geo_extractor.downloaders import (
     BellingcatDownloader,
     CenInfoResDownloader,
@@ -8,21 +9,25 @@ from geo_extractor.downloaders import (
 # XXX: Running this requires an active internet connection
 #      Downloading will take ~25Mb
 
+@pytest.mark.skip(reason='Downloads large files')
 def test_defmon_downloader():
     d = DefmonDownloader()
     data = d.download()
     assert 'meta' in data
 
+@pytest.mark.skip(reason='Downloads large files')
 def test_bellingcat_downloader():
     d = BellingcatDownloader()
     data = d.download()
     assert data[0]['id'] == 'CIV0001'
 
+@pytest.mark.skip(reason='Downloads large files')
 def test_ceninfores_downloader():
     d = CenInfoResDownloader()
     data = d.download()
     assert data['geojson']['type'] == 'FeatureCollection'
 
+@pytest.mark.skip(reason='Downloads large files')
 def test_geoconfirmed_downloader():
     d = GeoConfirmedDownloader()
     data = d.download()
