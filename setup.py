c47d10e3d25965a2957587fbe87d4d29a96ed211
Anonymous <none@none.none>
2022-10-21 16:01:31 +0200

    setup.py: Bump to 0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (parent / "README.md").read_text()
 setup(
     name='osint-geo-extractor',
     author='conflict-investigations',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
