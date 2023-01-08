9696b55f58d35e8e37d75c2eefe9eb9d591a3a25
Anonymous <none@none.none>
2023-01-08 18:31:22 +0100

    downloaders: defmon_spreadsheet: Use updated URL
    The previous Google Docs export endpoint was only emitting
    around 50 lines of data.

--- geo_extractor/downloaders/defmon_spreadsheet.py
+++ geo_extractor/downloaders/defmon_spreadsheet.py
@@ -1,6 +1,6 @@
 from .base import Downloader
 
-DEFMON_CSV_ENDPOINT = 'https://docs.google.com/spreadsheets/d/1eNjacqBz3nbbebasDw0VtQcX5ZscRP7pRlkSICBt2aU/gviz/tq?tqx=out:csv'  # noqa
+DEFMON_CSV_ENDPOINT = 'https://docs.google.com/spreadsheets/d/1eNjacqBz3nbbebasDw0VtQcX5ZscRP7pRlkSICBt2aU/export?format=csv'  # noqa
 
 class DefmonSpreadsheetDownloader(Downloader):
 
