d0ca9394bd54488cec06097afbfcdef56d4531a9
Anonymous <none@none.none>
2023-03-12 15:35:51 +0100

    downloaders: geoconfirmed: Update endpoint URL

--- geo_extractor/downloaders/geoconfirmed.py
+++ geo_extractor/downloaders/geoconfirmed.py
@@ -3,12 +3,14 @@ from typing import Any
 
 from .base import Downloader
 
-GEOCONFIRMED_ENDPOINT = 'https://geoconfirmed.azurewebsites.net/api/map/GetMap/Ukraine'  # noqa
+GEOCONFIRMED_ENDPOINT = 'https://geoconfirmed.azurewebsites.net/api/placemark/Ukraine'  # noqa
+# Also available:
+# https://geoconfirmed.azurewebsites.net/api/placemark/Ukraine
 
 class GeoConfirmedDownloader(Downloader):
     """
     @GeoConfirmed
-    Data from https://geoconfirmed.azurewebsites.net/api/map/GetMap/Ukraine
+    Data from https://geoconfirmed.azurewebsites.net/api/placemark/Ukraine
     with blessing from author
     """
 
